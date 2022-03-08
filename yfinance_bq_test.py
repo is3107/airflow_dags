6caf712b07aab25a02668ffeea5b38150b298cf7
dlsh <dlsh3858@gmail.com>
2022-03-09 04:32:03 +0800

    Initial commit

---
+++ yfinance_bq_test.py
@@ -0,0 +1,72 @@
+from airflow.decorators import dag, task
+
+import logging
+
+from datetime import datetime
+from datetime import date
+
+from google.cloud import bigquery
+
+import yfinance as yf
+import pandas as pd
+
+
+default_args={
+    'owner': 'is3107',
+    'retries': 5
+}
+
+@dag(
+    default_args=default_args,
+    description='Test Data yfinance pipeline',
+    schedule_interval=None,
+    start_date=datetime(2022, 3, 1),
+    catchup=False,
+    tags=['yfinance'],
+)
+
+def yfinance_bq_test_etl():
+
+    @task
+    def yfinance2bq_apple_daily(ds=None):
+
+        # Download data
+        logging.info("Downloading Data")
+        data=yf.download("AAPL", start='2022-03-01', end='2022-03-05')
+        logging.info("Download Completed")
+
+        # Fix indexes
+        logging.info("Fixing data indexes and adding p_date column")
+        data.reset_index(inplace=True)
+        data.columns=data.columns.str.lower()
+        data.columns=data.columns.str.replace(' ', '_')
+
+        # Add partition date column
+        data['p_date']=ds
+
+        # Remove old partition data from BigQuery
+        logging.info("Deleting old data from partition")
+        client=bigquery.Client()
+        query=f"DELETE is3107.ods.ods_yfinance_apple_daily WHERE p_date = \"{ds}\""
+        query_job=client.query(query)
+        query_job.result()
+
+        # Load new data to BigQuery
+        logging.info("Loading new data into partition")
+        data.to_gbq('ods.ods_yfinance_apple_daily', if_exists='append', table_schema=[
+            {
+                'name': 'date',
+                'type': 'DATETIME'
+            },
+            {
+                'name': 'p_date',
+                'type': 'DATE'
+            }
+        ])
+        
+
+    yfinance2bq = yfinance2bq_apple_daily()
+
+yfinance_bq_test_etl_dag = yfinance_bq_test_etl()
+
+        
