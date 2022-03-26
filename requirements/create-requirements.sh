80823aee649274437fc8878dcd017218e0799276
dlsh <dlsh3858@gmail.com>
2022-03-26 13:01:16 +0000

    Fix requirements pre-commit

--- requirements/create-requirements.sh
+++ requirements/create-requirements.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if [[ -z "$VIRTUAL_ENV" ]]; then
-    source /home/is3107/is3107/bin/activate 
+  source /home/is3107/is3107/bin/activate 
 fi
+
+rm requirements/requirements.txt
 
 pip freeze | grep --invert-match pkg_resources > requirements/requirements.txt
 
@@ -10,5 +12,6 @@ while [ ! -f requirements/requirements.txt ]
 do
   sleep 0.1
 done
+
 
 git add requirements/requirements.txt
